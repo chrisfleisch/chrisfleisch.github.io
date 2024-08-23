95480fa6998b5caf0758969c527780235e7be4dc
Chris Fleisch <chris@simonsfoundation.org>
2024-08-23 13:52:35 -0400

    Added prettier to fe

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import MyGallery from './components/Photos';
-import SiteFooter from './components/Footer';
+import MyGallery from "./components/Photos";
+import SiteFooter from "./components/Footer";
 import data from "./data/photos.json";
-
 
 function App() {
   return (
     <>
-      <header className='site-header'>
+      <header className="site-header">
         <div className="container">
-          <a className="site-title" href="/">Chris Fleisch</a>
+          <a className="site-title" href="/">
+            Chris Fleisch
+          </a>
         </div>
       </header>
       <div className="container page-content">
--- fe/src/components/Footer.js
+++ fe/src/components/Footer.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { SocialIcon } from 'react-social-icons'
-
+import { SocialIcon } from "react-social-icons";
 
 export default function SiteFooter() {
-    return (
-        <footer>
-            <div className='container'>
-                <SocialIcon url="https://www.flickr.com/photos/chrisfleisch" />
-                <SocialIcon url="https://www.linkedin.com/in/chris-fleisch" />
-                <SocialIcon url="https://github.com/chrisfleisch" />
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="container">
+        <SocialIcon url="https://www.flickr.com/photos/chrisfleisch" />
+        <SocialIcon url="https://www.linkedin.com/in/chris-fleisch" />
+        <SocialIcon url="https://github.com/chrisfleisch" />
+      </div>
+    </footer>
+  );
 }
